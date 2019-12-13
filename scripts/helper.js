31f5308434c515d434fdae6031f4c2b4ad99261f
Ryan Lee <ryology231@gmail.com>
2019-12-12 22:08:57 -0800

    Lessons page developed. Added buttons that can be linked to published slides. Blender slides published and linked to.

--- scripts/helper.js
+++ scripts/helper.js
@@ -6,3 +6,18 @@ function buttonFade() {
 function buttonShow() {
 
 }
+
+var links = {
+  sculpting: "https://docs.google.com/presentation/d/e/2PACX-1vTszCzRxCCJ7-JTpbHpUVSvTYJ7MID24PBhwvgY2M0qYkUF0BscYGzGl6rxn7wKkC-ap0IeWNQTRBUX/pub?start=false&loop=false&delayms=3000",
+  painting: "https://docs.google.com/presentation/d/e/2PACX-1vRets4WF_lfCmrv00ZxLr08_8FlH2ZUxeCDEvc7sRhf7QqZaVWcZ6H1GGwNJWfurpKTkgN7p8kp9c5S/pub?start=false&loop=false&delayms=3000",
+  animating: "https://docs.google.com/presentation/d/e/2PACX-1vQr_ZzfxG3d6MIjXhmTKX3Gnx6B9aErdo4dJb9MM09-utsWHyMJOgQX9zf7-h9xHOBnRB6Eb3goEYna/pub?start=false&loop=false&delayms=3000"
+}
+
+function openLesson(lessonName) {
+  if (links.hasOwnProperty(lessonName)) {
+    window.open(links[lessonName]);
+  } else
+  {
+    console.log("Lesson does not exist");
+  }
+}
