8837631c652d4c6257d8f53e033c87b9d7b2193f
Ryan Lee <ryology231@gmail.com>
2019-12-20 13:54:32 -0800

    Added div to push other elements below the fixed navbar

--- scripts/queryfuncs.js
+++ scripts/queryfuncs.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
-  $("header").load("html-snippets/navbar.html");
+  $("header").load("html-snippets/navbar.html", function() {
+    var navHeight = document.getElementById("navbar").offsetHeight;
+    console.log("Nav: " + navHeight);
+    $("#page-depressant").css("height", "" + (navHeight + 20));
+  });
+
+
+  var $window = $(window);
+  var width = $window.width();
+  var height = $window.height();
+
+  setInterval(function () {
+      if ((width != $window.width()) || (height != $window.height())) {
+          width = $window.width();
+          height = $window.height();
+
+          var navHeight = document.getElementById("navbar").offsetHeight;
+          console.log("Nav: " + navHeight);
+          $("#page-depressant").css("height", "" + navHeight);
+      }
+  }, 300);
 })
