bcbedaaff93bd454037e962b9b14f9ac258bb620
Ryan Lee <ryology231@gmail.com>
2019-12-20 12:11:30 -0800

    Revised lessons page format, moved header code to a separate file.

--- scripts/queryfuncs.js
+++ scripts/queryfuncs.js
@@ -1,7 +1,3 @@
 $(document).ready(function() {
-  $(".menu-button").hover(function() {
-    $(this).css("background-color", "rgba(0, 0, 0, 0.3)")
-  }, function() {
-    $(this).css("background-color", "rgba(0, 0, 0, 0)")
-  });
+  $("header").load("html-snippets/navbar.html");
 })
