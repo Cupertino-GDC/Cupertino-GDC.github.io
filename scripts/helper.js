7a934baf199565a0b0214683faa18a923d386e7e
Ryan Lee <ryology231@gmail.com>
2020-01-14 21:28:05 -0800

    Added JSON file with lesson link data, for ease of changing Lessons page

--- scripts/helper.js
+++ scripts/helper.js
@@ -7,10 +7,10 @@ var links = {
   Animator: "https://docs.google.com/presentation/d/e/2PACX-1vRYsQ8c7fcGUvh0BEXbyGcb9yaTZvF9cfMbIFldOZqSLhSaNKAythwMPdIqqSZiD5C5OlO7vSW57eQo/pub?start=false&loop=false&delayms=3000",
   Physics: "https://docs.google.com/presentation/d/e/2PACX-1vT-I00Alxti7bAKhmaZp8GnZIj98hoi-QsbUtTzhk_HTAXBucd-GmljPL4jwx3ifMieJLxI_pyXwlGY/pub?start=false&loop=false&delayms=3000",
   UISceneManager: "https://docs.google.com/presentation/d/e/2PACX-1vSgIjLRiUzCcVhH4S2p9CH_drquCnIi2qY2WEQmjqvHWzOaAzTZv7GtNtMbx6hd_huS5z12c28_OhUS/pub?start=false&loop=false&delayms=3000",
-  LightsnEffects: "https://docs.google.com/presentation/d/e/2PACX-1vRDZTdxpBfdygxusDLloaQhFJgP1zOF33HRBXki95l0Go5LIur7jPVq4DtBGj0RtRx21Jhs4ae4YOhx/pub?start=false&loop=false&delayms=3000",
   sculpting: "https://docs.google.com/presentation/d/e/2PACX-1vTszCzRxCCJ7-JTpbHpUVSvTYJ7MID24PBhwvgY2M0qYkUF0BscYGzGl6rxn7wKkC-ap0IeWNQTRBUX/pub?start=false&loop=false&delayms=3000",
   painting: "https://docs.google.com/presentation/d/e/2PACX-1vRets4WF_lfCmrv00ZxLr08_8FlH2ZUxeCDEvc7sRhf7QqZaVWcZ6H1GGwNJWfurpKTkgN7p8kp9c5S/pub?start=false&loop=false&delayms=3000",
   animating: "https://docs.google.com/presentation/d/e/2PACX-1vQr_ZzfxG3d6MIjXhmTKX3Gnx6B9aErdo4dJb9MM09-utsWHyMJOgQX9zf7-h9xHOBnRB6Eb3goEYna/pub?start=false&loop=false&delayms=3000",
+  LightsnEffects: "https://docs.google.com/presentation/d/e/2PACX-1vRDZTdxpBfdygxusDLloaQhFJgP1zOF33HRBXki95l0Go5LIur7jPVq4DtBGj0RtRx21Jhs4ae4YOhx/pub?start=false&loop=false&delayms=3000",
   cinemachine: "https://docs.google.com/presentation/d/e/2PACX-1vT33YuTNqDICgII9KzAM59p683O-khvPoelzh1kbSVtOJpJrcKMy4LQ4m_hGtqEHca4FgPvPVZsOk02/pub?start=false&loop=false&delayms=3000"
 }
 
